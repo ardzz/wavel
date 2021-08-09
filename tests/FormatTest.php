52c9e43fc5b9d7731d78e47565c2c9f80e84a3bb
ardzz <ardzz@indoxploit.or.id>
2021-08-09 10:52:22 +0700

    Add new test for method `number` from 'Ardzz\Wavel\Cores::Format'

--- tests/FormatTest.php
+++ tests/FormatTest.php
@@ -2,20 +2,28 @@
 
 use Ardzz\Wavel\Cores\Format;
 
-test('format group id', function () {
-    expect(Format::number('00000-1111@g.us', true))->toBe('00000-1111@g.us');
+test('format group id (with tag identification)', function () {
+    expect(Format::number('00000-1111@g.us'))->toBe('00000-1111@g.us');
 });
 
-test('format user id', function () {
+test('format group id (without tag identification)', function () {
+    expect(Format::number('00000-1111'))->toBe('00000-1111@g.us');
+});
+
+test('format user id (without tag identification)', function () {
     expect(Format::number('000000'))->toBe('000000@c.us');
 });
 
+test('format user id (with tag identification)', function () {
+    expect(Format::number('000000@c.us'))->toBe('000000@c.us');
+});
+
 it('can format from document to be data url (data:%s;base64,%s)', function (){
-    expect(Format::document(__DIR__ . '/../sample_file'))->toBe('data:text/plain;base64,dGVzdENhc2UK');
+    expect(Format::document(__DIR__ . '/sample_file'))->toBe('data:text/plain;base64,dGVzdENhc2U=');
 });
 
 it('can format image to data url and validate that input is image', function(){
-    expect(Format::image(__DIR__ . '/../sample_file'))->toBeFalse();
+    expect(Format::image(__DIR__ . '/sample_file'))->toBeFalse();
 });
 
 it('can format participant group', function(){
